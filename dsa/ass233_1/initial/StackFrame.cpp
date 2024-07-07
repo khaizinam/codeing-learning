5f8f52ae5130d229a9429290f94c4c0ccde56a4a
khai.nh | apikey:22ec72502624e93d458c883992da7d9ac3e549fcbc3fdd254b917f3970bf395c <khai.nh@fsofts.com>
2024-07-07 21:50:34 +0700

    save

--- dsa/ass233_1/initial/StackFrame.cpp
+++ dsa/ass233_1/initial/StackFrame.cpp
@@ -5,8 +5,132 @@
 #include "constants.h"
 using namespace std;
 
-StackFrame::StackFrame() : opStackMaxSize(OPERAND_STACK_MAX_SIZE), localVarArrSize(LOCAL_VARIABLE_ARRAY_SIZE) {}
+// Default constructor
+Node::Node() : val(0), type(0), next(nullptr) {}
+
+// Constructor with parameters
+Node::Node(float value, int nodeType) : val(value), type(nodeType), next(nullptr) {}
+
+// Destructor
+Node::~Node() {
+    // No dynamic memory cleanup needed in this example
+}
+
+LinkedList::LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+void LinkedList::push(Node* newNode) {
+    if (head == nullptr) {
+        this->head = newNode;
+        this->tail = newNode;
+    } else {
+        this->tail->next = newNode;
+        this->tail = newNode;
+    }
+
+    cout << "insert Node(" << newNode->val << "," << newNode->type << ");\n";
+
+    size++;
+}
+
+Node* LinkedList::pop() {
+    if (this->head == nullptr) {
+        return nullptr;
+    }
+
+    Node* current = this->head;
+    Node* previous = nullptr;
+
+    while (current->next != nullptr) {
+        previous = current;
+        current = current->next;
+    }
+
+    if (previous != nullptr) {
+        previous->next = nullptr;
+        this->tail = previous;
+    } else {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    this->size--;
+    return current;
+}
+
+Node* LinkedList::top() {
+    return this->tail;
+}
+
+Node* LinkedList::getIndex(int index) {
+    if (index < 0 || index >= this->size) {
+        return nullptr;
+    }
+
+    Node* current = this->head;
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+
+    return current;
+}
+
+LinkedList::~LinkedList() {
+    // Destructor: Clean up all nodes in the list
+    Node* current = head;
+    Node* next = nullptr;
+    while (current != nullptr) {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+
+StackFrame::StackFrame() : opStackMaxSize(OPERAND_STACK_MAX_SIZE), localVarArrSize(LOCAL_VARIABLE_ARRAY_SIZE), stack(new LinkedList()), memory(new LinkedList()) {}
+
+void StackFrame::iconst(float val) { 
+    Node* newNode = new Node(val, 0);
+    this->stack->push(newNode);
+}
+
+void StackFrame::fconst(float val) { 
+    Node* newNode = new Node(val, 1);
+    this->stack->push(newNode);
+}
+
+void StackFrame::top(){
+    Node* tmp = this->stack->top();
+    if(tmp->type == 1)
+        cout << (int)tmp->val << "\n";
+    else 
+        cout << tmp->val << "\n";
+}
 
 void StackFrame::run(string filename) {
-    cout << filename << "\n";
+
+    std::fstream file(filename, std::ios::in); // Open the file for reading
+
+    if (!file.is_open()) { return; }
+
+    std::string javmCode;
+    float value = 0.0f;
+
+    while (file >> javmCode) { // Read each "word" (token) from the file
+        // Create Node objects based on javmCode and value
+        if (javmCode == "iconst") {
+            file >> value; 
+            this->iconst(value);
+
+        } else if(javmCode == "fconst"){
+            file >> value; 
+            this->fconst(value);
+
+        } else if(javmCode == "top") {
+            this->top();
+        }
+
+        // Add more conditions for other javmCode types if needed
+    }
+
+    file.close(); // Close the file
 }
--- dsa/ass233_1/initial/StackFrame.h
+++ dsa/ass233_1/initial/StackFrame.h
@@ -3,12 +3,52 @@
 
 #include <string>
 
+#define INTEGER_TYPE 0
+#define FLOAT_TYPE 1 
+/*
+Node
+*/
+class Node{
+public:
+    float val;
+    int type; // 0 : int , 1: flat
+    Node* next;
+
+public:
+    Node();
+
+    Node(float val, int type);
+
+    ~Node();
+};
+
+class LinkedList{
+public:
+    Node* head;
+    Node* tail;
+    int size;
+public:
+    LinkedList();
+
+    void push(Node* newNode); // push bash, increse size by 1;
+
+    Node* pop(); // create new Node from tail -> return that node -> delete tail;
+
+    Node* top(); // create new Node from tail -> return that node;
+
+    Node* getIndex(int index); // get Node from index start from head;
+
+    ~LinkedList();
+};
+
 /*
 StackFrame declaration
 */
 class StackFrame {
     int opStackMaxSize; // max size of operand stack
     int localVarArrSize; // size of local variable array
+    LinkedList* stack;
+    LinkedList* memory;
 public:
     /*
     Constructor of StackFrame
@@ -20,6 +60,14 @@ public:
     @param filename name of the file
     */
     void run(std::string filename);
+
+    void iconst(float val);
+
+    void fconst(float val);
+
+    void fconst(float val);
+    
+    void top();
 };
 
 #endif // !__STACK_FRAME_H__
